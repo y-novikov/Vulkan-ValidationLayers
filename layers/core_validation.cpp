9a5827f9e519834747841202b384c6532f01e661
Jeremy Kniager <jeremyk@lunarg.com>
2020-04-01 09:05:56 -0600

    layers: Add combined image sampler check
    Added check to descriptor_sets.cpp for the following:
    If the VkFilter used to create a VkSampler is not supported
    by the VkFormat used to create a VkImageView, the VkSampler
    and VkImageView must not be used in the same Combined Image Sampler
    descriptor set.

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -33,6 +33,7 @@
  * Author: Tony Barbour <tony@LunarG.com>
  * Author: John Zulauf <jzulauf@lunarg.com>
  * Author: Shannon McPherson <shannon@lunarg.com>
+ * Author: Jeremy Kniager <jeremyk@lunarg.com>
  */
 
 #include <algorithm>
@@ -891,6 +892,8 @@ bool CoreChecks::ValidateCmdBufDrawState(const CMD_BUFFER_STATE *cb_node, CMD_TY
                                                     state.per_set[setIndex].validated_set_binding_req_map.end(),
                                                     binding_req_map.begin(), binding_req_map.end());
 
+                struct DescriptorSetBindingDataVuid dsbd_vuids = {vuid.linear_sampler, vuid.cubic_sampler};
+
                 if (need_validate) {
                     if (!descriptor_set_changed && reduced_map.IsManyDescriptors()) {
                         // Only validate the bindings that haven't already been validated
@@ -900,10 +903,10 @@ bool CoreChecks::ValidateCmdBufDrawState(const CMD_BUFFER_STATE *cb_node, CMD_TY
                                             state.per_set[setIndex].validated_set_binding_req_map.end(),
                                             std::inserter(delta_reqs, delta_reqs.begin()));
                         result |= ValidateDrawState(descriptor_set, delta_reqs, state.per_set[setIndex].dynamicOffsets, cb_node,
-                                                    setIndex, function);
+                                                    setIndex, function, &dsbd_vuids);
                     } else {
                         result |= ValidateDrawState(descriptor_set, binding_req_map, state.per_set[setIndex].dynamicOffsets,
-                                                    cb_node, setIndex, function);
+                                                    cb_node, setIndex, function, &dsbd_vuids);
                     }
                 }
             }
@@ -2370,11 +2373,13 @@ bool CoreChecks::ValidateCommandBuffersForSubmit(VkQueue queue, const VkSubmitIn
                 if (set_node) {
                     for (auto pipe : descriptorSet.second) {
                         for (auto binding : pipe.second) {
+                            struct DescriptorSetBindingDataVuid dsbd_vuids = {kVUID_Core_DrawState_DescriptorSetNotUpdated,
+                                                                              kVUID_Core_DrawState_DescriptorSetNotUpdated};
                             std::string error;
                             std::vector<uint32_t> dynamicOffsets;
                             // dynamic data isn't allowed in UPDATE_AFTER_BIND, so dynamicOffsets is always empty.
                             skip |= ValidateDescriptorSetBindingData(cb_node, set_node, dynamicOffsets, binding.first,
-                                                                     binding.second, "vkQueueSubmit()");
+                                                                     binding.second, "vkQueueSubmit()", &dsbd_vuids);
                         }
                     }
                 }
